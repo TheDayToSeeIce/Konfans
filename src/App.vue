62cc726cbed5d9baa2029fe75273428867318b40
Chaoxip <99881428+TheDayToSeeIce@users.noreply.github.com>
2022-08-12 15:04:25 +0800

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,39 @@
+<template>
+  <div id="app">
+    <!-- <img alt="Vue logo" src="./assets/logo.png"> -->
+    <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+    <router-view></router-view>
+    
+  </div>
+</template>
+
+<script>
+// import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'App',
+  // components: {
+  //   HelloWorld
+  // }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  background-color:white;
+  margin: 0px;
+  padding: 0px;
+}
+ * {
+  margin: 0px;
+  padding: 0px;
+ }
+</style>
